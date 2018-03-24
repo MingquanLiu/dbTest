c9c4db359f651a7689c3bf585695da1bc48d41aa
epicderek <31416815+epicderek@users.noreply.github.com>
2018-03-25 07:42:37 +0800

    Update SQLiteActivity.java

--- app/src/main/java/com/example/mikel/dbtest/SQLiteActivity.java
+++ app/src/main/java/com/example/mikel/dbtest/SQLiteActivity.java
@@ -27,8 +27,9 @@ public class SQLiteActivity extends Activity
 
         public void wrapUp(Place loc)
         {
-            db.addPlace(loc);
             Log.v("See",loc.toString());
+            db.addPlace(loc);
+            Log.v("See",db.getAllPlaces().toString());
         }
     }
     @Override
